db417b9dcd51eac920bcfd874f2ad2ebaedc7b58
Matheus Ochi <mths.ochi@gmail.com>
2021-04-21 09:47:11 -0300

    2nd day class

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from "../components/Header";
+
+export default function Home(props) {
+  console.log(props.episodes);
+
+  return (
+    <h1>Index</h1>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
